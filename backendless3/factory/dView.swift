c46a0caafae622a90f33ab91ea47640b2164d41f
hackeru <hackeru@hackerus-Mac.local>
2017-03-26 01:03:15 +0200

    test

--- backendless3/factory/dView.swift
+++ backendless3/factory/dView.swift
@@ -1,4 +1,5 @@
 public protocol dView{
+   //test
    func setProperties(xPosition:CGFloat , width:CGFloat , link:String , tag:Int)
    func setProperties(xPosition:CGFloat , width:CGFloat , title:String)
 }
